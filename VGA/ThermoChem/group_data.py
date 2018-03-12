dd3bbe88d319d149d2f307335791f612f04331d3
googhgoo <googhgoo@hotmail.com>
2018-03-12 18:41:28 -0400

    CItoSE
    SE is printed out instead of CI.

--- VGA/ThermoChem/group_data.py
+++ VGA/ThermoChem/group_data.py
@@ -2,7 +2,6 @@ from .. ThermoChem import ThermochemBase, ThermochemIncomplete
 from .. import yaml_io
 import numpy as np
 from ..GroupAdd.Library import GroupLibrary
-from scipy import stats
 
 __all__ = ['ThermochemGroup', 'ThermochemGroupAdditive']
 
@@ -83,17 +82,14 @@ class ThermochemGroupAdditive(ThermochemBase):
             for (correlation,count) in self.correlations))
     eval_ND_S.__doc__ = ThermochemBase.eval_ND_S.__doc__
     
-    def eval_ND_Cp_CI(self, T, pt): #pt is percentile
-        se = float(np.sqrt(np.square(self.RMSE.eval_ND_Cp(T))*self.Xp_invXX_Xp))
-        return stats.t.ppf(1-(100.0-pt)/200.0,self.dof)*se
+    def eval_ND_Cp_SE(self, T):
+        return float(np.sqrt(np.square(self.RMSE.eval_ND_Cp(T))*self.Xp_invXX_Xp))
             
-    def eval_ND_H_CI(self, T, pt):
-        se = float(np.sqrt(np.square(self.RMSE.eval_ND_H(T))*self.Xp_invXX_Xp))
-        return stats.t.ppf(1-(100.0-pt)/200.0,self.dof)*se
+    def eval_ND_H_SE(self, T):
+        return float(np.sqrt(np.square(self.RMSE.eval_ND_H(T))*self.Xp_invXX_Xp))
     
-    def eval_ND_S_CI(self, T, pt):
-        se = float(np.sqrt(np.square(self.RMSE.eval_ND_S(T))*self.Xp_invXX_Xp))
-        return stats.t.ppf(1-(100.0-pt)/200.0,self.dof)*se
+    def eval_ND_S_SE(self, ):
+        return float(np.sqrt(np.square(self.RMSE.eval_ND_S(T))*self.Xp_invXX_Xp))
 
 
 
