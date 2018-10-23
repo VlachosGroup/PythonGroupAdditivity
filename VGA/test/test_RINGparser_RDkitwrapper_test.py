72f1d21f55a6ee00e2599b7c129a10c14e1e4b41
Gerhard R Wittreich, P.E <wittregr@udel.edu>
2018-10-23 13:37:14 -0400

    Update unit tests

--- VGA/test/test_RINGparser_RDkitwrapper_test.py
+++ VGA/test/test_RINGparser_RDkitwrapper_test.py
@@ -46,7 +46,7 @@ class TestRINGParser(unittest.TestCase):
                                        (2, 3), (3, 2), (3, 4), (4, 3), (4, 5),
                                        (5, 0), (5, 4)))
 
-    def test_ring_bond(self):
+    def test_ring_bond1(self):
         testmol = Chem.MolFromSmiles('CCC1CCC1')
         s = """
         fragment a{
@@ -63,6 +63,312 @@ class TestRINGParser(unittest.TestCase):
                                        (3, 2, 5, 4), (3, 4, 5, 2),
                                        (4, 3, 2, 5), (4, 5, 2, 3),
                                        (5, 2, 3, 4), (5, 4, 3, 2)))
+
+    def test_ring_bond2(self):
+        testmol = Chem.MolFromSmiles('CCC1CCC1')
+        s = """
+        fragment a{
+            C labeled c1
+            C labeled c2 ring bond to c1
+        }
+        """
+        molquery = Read(s)
+        match_index = molquery.GetQueryMatches(testmol)
+        self.assertEqual(match_index, ((2, 3), (2, 5), (3, 2), (3, 4), (4, 3),
+                                       (4, 5), (5, 2), (5, 4)))
+
+    def test_non_ring_bond(self):
+        testmol = Chem.MolFromSmiles('CCC1CCC1')
+        s = """
+        fragment a{
+            C labeled c1
+            C labeled c2 nonring bond to c1
+        }
+        """
+        molquery = Read(s)
+        match_index = molquery.GetQueryMatches(testmol)
+        self.assertEqual(match_index, ((0, 1), (1, 0), (1, 2), (2, 1)))
+
+    def test_any_bond1(self):
+        testmol = Chem.MolFromSmiles('CC=CC#C')
+        s = """
+        fragment a{
+            C labeled c1
+            C labeled c2 any bond to c1
+        }
+        """
+        molquery = Read(s)
+        match_index = molquery.GetQueryMatches(testmol)
+        self.assertEqual(match_index, ((0, 1), (1, 0), (1, 2), (2, 1),
+                                       (2, 3), (3, 2), (3, 4), (4, 3)))
+
+    def test_any_bond2(self):
+        testmol = Chem.MolFromSmiles('CC=CC#C')
+        s = """
+        fragment a{
+            C labeled c1
+            C labeled c2 any bond to c1
+        }
+        """
+        molquery = Read(s)
+        match_index = molquery.GetQueryMatches(testmol)
+        self.assertEqual(match_index, ((0, 1), (1, 0), (1, 2), (2, 1),
+                                       (2, 3), (3, 2), (3, 4), (4, 3)))
+
+    def test_strong_bond(self):
+        testmol = Chem.MolFromSmiles('CC=CC#C')
+        s = """
+        fragment a{
+            C labeled c1
+            C labeled c2 strong bond to c1
+        }
+        """
+        molquery = Read(s)
+        match_index = molquery.GetQueryMatches(testmol)
+        self.assertEqual(match_index, ((1, 2), (2, 1), (3, 4), (4, 3)))
+
+    def test_other_bond1(self):
+        testmol = Chem.MolFromSmiles('C[CH2-]')
+        s = """
+        positive fragment a{
+            C labeled c1
+            C labeled c2 single bond to c1
+        }
+        """
+        molquery = Read(s)
+        match_index = molquery.GetQueryMatches(testmol)
+        self.assertEqual(match_index, ())
+
+    def test_other_bond2(self):
+        testmol = Chem.MolFromSmiles('C[CH2+]')
+        s = """
+        negative fragment a{
+            C labeled c1
+            C labeled c2 single bond to c1
+        }
+        """
+        molquery = Read(s)
+        match_index = molquery.GetQueryMatches(testmol)
+        self.assertEqual(match_index, ())
+
+    def test_other_bond3(self):
+        testmol = Chem.MolFromSmiles('C=CC')
+        s = """
+        olefinic fragment a{
+            C labeled c1
+            C labeled c2 single bond to c1
+        }
+        """
+        molquery = Read(s)
+        match_index = molquery.GetQueryMatches(testmol)
+        self.assertEqual(match_index, ((1,2),(2,1)))
+
+    def test_other_bond4(self):
+        testmol = Chem.MolFromSmiles('C=CC')
+        s = """
+        paraffinic fragment a{
+            C labeled c1
+            C labeled c2 single bond to c1
+        }
+        """
+        molquery = Read(s)
+        match_index = molquery.GetQueryMatches(testmol)
+        self.assertEqual(match_index, ())
+
+    def test_other_bond5(self):
+        testmol = Chem.MolFromSmiles('CCC')
+        s = """
+        paraffinic fragment a{
+            C labeled c1
+            C labeled c2 single bond to c1
+        }
+        """
+        molquery = Read(s)
+        match_index = molquery.GetQueryMatches(testmol)
+        self.assertEqual(match_index, ((0, 1), (1, 0), (1, 2), (2, 1)))
+
+    def test_other_bond6(self):
+        testmol = Chem.MolFromSmiles('CCC')
+        s = """
+        linear fragment a{
+            C labeled c1
+            C labeled c2 single bond to c1
+        }
+        """
+        molquery = Read(s)
+        match_index = molquery.GetQueryMatches(testmol)
+        self.assertEqual(match_index, ((0, 1), (1, 0), (1, 2), (2, 1)))
+
+    def test_other_bond7(self):
+        testmol = Chem.MolFromSmiles('CCC')
+        s = """
+        cyclic fragment a{
+            C labeled c1
+            C labeled c2 single bond to c1
+        }
+        """
+        molquery = Read(s)
+        match_index = molquery.GetQueryMatches(testmol)
+        self.assertEqual(match_index, ())
+
+    def test_other_bond8(self):
+        testmol = Chem.MolFromSmiles('C1CCC1C')
+        s = """
+        cyclic fragment a{
+            C labeled c1
+            C labeled c2 single bond to c1
+        }
+        """
+        molquery = Read(s)
+        match_index = molquery.GetQueryMatches(testmol)
+        self.assertEqual(match_index, ((0, 1), (0, 3), (1, 0), (1, 2),
+                                       (2, 1), (2, 3), (3, 0), (3, 2),
+                                       (3, 4), (4, 3)))
+
+    def test_symbol_atomsuffix(self):
+        testmol = Chem.MolFromSmiles('CCC')
+        s = """
+        fragment a{
+            $ labeled c1
+            $ labeled c2 single bond to c1
+        }
+        """
+        molquery = Read(s)
+        match_index = molquery.GetQueryMatches(testmol)
+        self.assertEqual(match_index, ((0, 1), (0, 3), (0, 4), (0, 5), (1, 0),
+                                       (1, 2), (1, 6), (1, 7), (2, 1), (2, 8),
+                                       (2, 9), (2, 10), (3, 0), (4, 0),
+                                       (5, 0), (6, 1), (7, 1), (8, 2), (9, 2),
+                                       (10, 2)))
+    def test_other_bond8(self):
+        testmol = Chem.MolFromSmiles('CCO')
+        s = """
+        fragment a{
+            X labeled c1
+            X labeled c2 single bond to c1
+        }
+        """
+        molquery = Read(s)
+        match_index = molquery.GetQueryMatches(testmol)
+        self.assertEqual(match_index, ((0, 1), (1, 0), (1, 2), (2, 1)))
+
+    def test_other_bond10(self):
+        testmol = Chem.MolFromSmiles('CCS')
+        s = """
+        fragment a{
+            X labeled c1
+            & labeled c2 single bond to c1
+        }
+        """
+        molquery = Read(s)
+        match_index = molquery.GetQueryMatches(testmol)
+        self.assertEqual(match_index, ((1, 2),))
+
+    def test_atom_constraint1(self):
+        testmol = Chem.MolFromSmiles('CCC')
+        s = """
+        fragment a{
+            C labeled c1
+            C labeled c2 single bond to c1 {connected to =2 C}
+        }
+        """
+        molquery = Read(s)
+        match_index = molquery.GetQueryMatches(testmol)
+        self.assertEqual(match_index, ((0, 1), (2, 1)))
+
+    def test_atom_constraint2(self):
+        testmol = Chem.MolFromSmiles('CCC')
+        s = """
+        fragment a{
+            C labeled c1
+            C labeled c2 single bond to c1 {connected to =1 C}
+        }
+        """
+        molquery = Read(s)
+        match_index = molquery.GetQueryMatches(testmol)
+        self.assertEqual(match_index, ((1, 0), (1, 2)))
+
+    def test_atom_constraint3(self):
+        testmol = Chem.MolFromSmiles('CC=C')
+        s = """
+        fragment a{
+            C labeled c1
+            C labeled c2 single bond to c1 {connected to =1 C with double bond}
+        }
+        """
+        molquery = Read(s)
+        match_index = molquery.GetQueryMatches(testmol)
+        self.assertEqual(match_index, ((0, 1),))
+
+    def test_atom_constraint4(self):
+        testmol = Chem.MolFromSmiles('CC=C')
+        s = """
+        fragment a{
+            C labeled c1 {connected to >1 C with any bond}
+        }
+        """
+        molquery = Read(s)
+        match_index = molquery.GetQueryMatches(testmol)
+        self.assertEqual(match_index, ((1,),))
+
+    def test_atom_constraint5(self):
+        testmol = Chem.MolFromSmiles('CC=C')
+        s = """
+        fragment a{
+            C labeled c1 {!connected to >1 C with any bond}
+        }
+        """
+        molquery = Read(s)
+        match_index = molquery.GetQueryMatches(testmol)
+        self.assertEqual(match_index, ((0,),(2,)))
+
+    def test_atom_constraint6(self):
+        testmol = Chem.MolFromSmiles('CC1CCC1')
+        s = """
+        fragment a{
+            C labeled c1 {!in ring of size >0}
+            C labeled c2 single bond to c1 {in ring of size >0}
+        }
+        """
+        molquery = Read(s)
+        match_index = molquery.GetQueryMatches(testmol)
+        self.assertEqual(match_index, ((0, 1),))
+
+    def test_atom_prefix1(self):
+        testmol = Chem.MolFromSmiles('CC1CCC1')
+        s = """
+        fragment a{
+            nonringatom C labeled c1
+            ringatom C labeled c2 single bond to c1
+        }
+        """
+        molquery = Read(s)
+        match_index = molquery.GetQueryMatches(testmol)
+        self.assertEqual(match_index, ((0, 1),))
+
+    def test_atom_prefix2(self):
+        testmol = Chem.MolFromSmiles('Cc1ccccc1')
+        s = """
+        fragment a{
+            nonaromatic C labeled c1
+            aromatic C labeled c2 single bond to c1
+        }
+        """
+        molquery = Read(s)
+        match_index = molquery.GetQueryMatches(testmol)
+        self.assertEqual(match_index, ((0, 1),))
+
+    def test_atom_prefix3(self):
+        testmol = Chem.MolFromSmiles('CC=C')
+        s = """
+        fragment a{
+            C labeled c1
+            allylic C labeled c2 single bond to c1
+        }
+        """
+        molquery = Read(s)
+        match_index = molquery.GetQueryMatches(testmol)
+        self.assertEqual(match_index, ((0, 1),))
 
 
 if __name__ == '__main__':
