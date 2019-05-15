436adb6072db30b7f3d5011b7c8ba50c256a6ad3
Gerhard R Wittreich, P.E <wittregr@udel.edu>
2019-05-15 17:25:10 -0400

    Fix name issues

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open('README.rst', 'r') as fh:
 
 setuptools_info = {
 	'name': 'pGrAdd',
-	'version': '1.3.8',
+	'version': '1.4',
 	'author': 'Vlachos Research Group',
 	'author_email': 'vlachos@udel.edu',
 	'description': 'Python package implements the Group Additivity (GA) method for estimating thermodynamic properties',
