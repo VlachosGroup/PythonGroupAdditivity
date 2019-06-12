8de2000092aa0971e67da49224a64601805f4c19
Marc Bolinas <bolinas.marc@gmail.com>
2019-06-12 14:15:18 -0400

    further const conversion to pmutt.constants

--- pGrAdd/ThermoChem/raw_data.py
+++ pGrAdd/ThermoChem/raw_data.py
@@ -17,7 +17,7 @@ class ThermochemRawData(ThermochemBase):
     Evaluated quantities are interpolated using a B-spline as discussed in
     :ref:`correlations documentation <correlations>`.
     """
-    def __init__(self, ND_H_ref, ND_S_ref, Ts, ND_Cps, T_ref=298.15,
+    def __init__(self, ND_H_ref, ND_S_ref, Ts, ND_Cps, T_ref=pmutt.constants.T0(units='K'),
                  range=None):
         """
         Initialize a thermochemical property correlation from raw data.
@@ -35,7 +35,7 @@ class ThermochemRawData(ThermochemBase):
             evaluated at each temperature in `Ts`.
         T_ref : float, optional
             Reference temperature |eq_Tref| for `ND_H_ref` and `ND_S_ref`
-            (default: 298.15K).
+            (default: room temperature according to pMuTT, likely 298.15K).
         range : tuple(float, float), optional
             ``(lb, ub) = range`` where lb and ub are respectively the lower and
              uppers bounds of temperatures [K] for which the correlation is
