33ffa362cbb5394d8604e33efd00e006838e7b1b
Gerhard R Wittreich, P.E <wittregr@udel.edu>
2019-11-14 08:57:55 -0500

    Bug fix in BensonGA
    Fixed misspelled word Gacueh to Gauech

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open('README.rst', 'r') as fh:
 
 setuptools_info = {
 	'name': 'pgradd',
-	'version': '1.5',
+	'version': '1.6',
 	'author': 'Vlachos Research Group',
 	'author_email': 'vlachos@udel.edu',
 	'description': 'Python package implements the Group Additivity (GA) method for estimating thermodynamic properties',
